69385d4f1da3656743731d670576f508284d21c6
Slava_Kamashev <g3ntlmen@yandex.ru>
2020-12-09 20:03:39 +0400

    upgrade calculator function

--- js/script.js
+++ js/script.js
@@ -485,20 +485,62 @@ document.addEventListener("DOMContentLoaded", () => {
 // CALCULATOR
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female', 
-    height, weight, age,
-    ratio = '1.375';
+    let sex, height, weight, age,  ratio;
+
+    // Устанавливаем данные в переменные из localStorage
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    // Функция для установления классов активности на кнопках
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+
+            // ПРоверяем дивы с гендером
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            
+            //Проверяем дивы с активностью 
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
     // Собираем данные о гендере и физической активности из ДОМ дерева
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    
+                    // Закидываем данные в local storage
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
+
                 } else {
                     sex = e.target.getAttribute('id');
+
+                    // Закидываем данные в local storage
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
                 }
     
                 elements.forEach(elem => {
@@ -511,14 +553,21 @@ document.addEventListener("DOMContentLoaded", () => {
         });      
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
     // Собираем данные инпутов(параметров тела) из ДОМ дерева
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            
+            if (input.value.match((/\D/g))) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch (input.getAttribute('id')) {
                 case 'height': 
                     height = +input.value;
@@ -547,7 +596,7 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
             
         }
-
+        // Формула рвсчёта значений
         if (sex === 'female') {
             result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
         } else {
